11a5a7e374a65fcd0bd41d437fd3737e67a2ecb4
Jason <jason@cole.mn>
2021-11-12 10:28:51 -0500

    New class to cull steal time on a separate thread.

---
+++ steal_queue.py
@@ -0,0 +1,63 @@
+from collections import deque
+import time
+import threading
+
+class StealQueue:
+    """Return the current steal time, or an average steal time for the
+    last 10 seconds, 1 minute, or 10 minutes.
+    """
+
+    # 1. Scrape steal time from 'vmstat' once per second.
+    # 2. Enqueue each steal time value.
+    # 3. When get_average() is called calculate the average steal time
+    #    for the given number of seconds.
+    #
+    # What's the largest number of seconds we will allow? I don't want to keep infinite data.
+    # Deque can have a max length and then allow the oldest data to slip out of the queue. That
+    # keeps you from having infinite data.
+
+    def __init__(self):
+        self.dq = deque(maxlen = 600)
+
+    def runProcess(exe):    
+        p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while(True):
+            # returns None while subprocess is running
+            retcode = p.poll() 
+            line = p.stdout.readline()
+            yield line
+            if retcode is not None:
+                break
+
+    
+
+    def get_average(self, seconds = 1):
+        pass
+
+    def get_10sec(self):
+        return self.get_average(10)
+
+    def get_1min(self):
+        return self.get_average(60)
+
+    def get_5min(self):
+        return self.get_average(300)
+
+    def get_10min(self):
+        return self.get_average(600)
+
+    def start(self):
+        for line in self.runProcess('/usr/bin/vmstat 1'.split()):
+            print(line)
+
+    def _main():
+        sq = StealQueue()
+        t1 = threading.Thread(target=sq.start)
+        t1.start()
+
+        while True:
+            time.sleep(1)
+            print('From main thread')
+    
+if __name__ == "__main__":
+    StealQueue._main()
