3f3023efb0417a409bbda12e8ece39eb4e6f7ae3
Jason <bassjas@gmail.com>
2021-11-12 20:42:38 -0500

    Finished the average() function

--- steal_queue.py
+++ steal_queue.py
@@ -20,6 +20,20 @@ class StealQueue:
     def __init__(self):
         self.dq = deque(maxlen = 600)
 
+    def get_average(self, seconds = 1):
+        """
+        Return the average stealtime over the last n seconds.
+        If we have less than n seconds of data use it all.
+        """
+        # It's an error if seconds is not >=1 and <=600
+        if seconds > len(self.dq):
+            seconds = len(self.dq)
+        mylist = list(self.dq)[:seconds]
+        total = 0
+        for i in mylist:
+            total = total + i
+        return int(total / seconds)
+
     def runProcess(self, exe):    
         p = subprocess.Popen(exe, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         while(True):
@@ -30,45 +44,29 @@ class StealQueue:
             if retcode is not None:
                 break 
 
-    def get_average(self, seconds = 1):
-        # It's an error if seconds is not >=1 and <=600
-        mylist = list(self.dq)[:seconds]
-        total = 0
-        for i in mylist:
-            total = total + i
-        return int(total / seconds)
-
-    def get_10sec(self):
-        return self.get_average(10)
-
-    def get_1min(self):
-        return self.get_average(60)
-
-    def get_5min(self):
-        return self.get_average(300)
-
-    def get_10min(self):
-        return self.get_average(600)
-
-    def start(self):
+    def loop(self):
+        """
+        Start the subprocess that will run vmstat.
+        Loop over the output and extract the stealtime values.
+        """
         for line in self.runProcess('/usr/bin/vmstat 1'.split()):
             line = line.rstrip()
             first, last = line.rsplit(maxsplit=1)
             try:
                 stealtime = int(last)
-                self.dq.append(stealtime)
+                self.dq.appendleft(stealtime)
                 print(self.dq)
             except ValueError:
                 pass # vmstat periodically prints column headers, not data
 
     def _main():
         sq = StealQueue()
-        t1 = threading.Thread(target=sq.start)
+        t1 = threading.Thread(target=sq.loop)
         t1.start()
 
         while True:
             time.sleep(3)
-            print('From main thread')
+            print('From main thread: average = {}'.format(sq.get_average(5)))
     
 if __name__ == "__main__":
     StealQueue._main()
